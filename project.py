13822863b4a613167fef779f9c9ff83b6dd72c43
Jakub Smejkal <smejkal.jakub99@gmail.com>
2020-04-19 20:55:41 +0200

    possible update

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import sys
 
 import numpy as np
 from time import time
@@ -15,10 +16,13 @@ from skimage.io import imread
 classifier = SVC(C=1e2, probability=True)
 
 
-trainData = {'targets': [],
+trainData = {'classes': [],
             'img_hog': [],
             'img_hog_d': []}
-testData = {'targets': [],
+testData = {'classes': [],
+            'img_hog': [],
+            'img_hog_d': []}
+evalData = {'classes': [],
             'img_hog': [],
             'img_hog_d': []}
 
@@ -26,52 +30,104 @@ images = {}
 
 
 
-def loadTrainDirData(dir, trainDir):
+def loadTrainDirImages(dir, trainDir):
     os.chdir(dir)
+    
+    subdirectories = os.listdir(dir)
+
+    for subdir in subdirectories:
+
+        os.chdir(dir + '/' + subdir)
+
+        completePath = dir + '/' + subdir
+
+        for file in glob.glob("*.png"):
+            filePath = completePath + '/' + file
+            print(filePath)
+            images[file] = imread(filePath, as_gray=True)
+            if(trainDir):
+
+                trainData['classes'].append(subdir)
+
+                fd, img = hog(images[file],
+                    orientations=8,
+                    pixels_per_cell=(8, 8),
+                    cells_per_block=(1, 1),
+                    block_norm='L1',
+                    visualize=True)
+                trainData['img_hog'].append(img)
+                trainData['img_hog_d'].append(fd)
+            else:
+                testData['classes'].append(subdir)
+
+                fd, img = hog(images[file],
+                    orientations=8,
+                    pixels_per_cell=(8, 8),
+                    cells_per_block=(1, 1),
+                    block_norm='L1',
+                    visualize=True)
+                testData['img_hog'].append(img)
+                testData['img_hog_d'].append(fd)
+
+def loadEvalDirImages(dir):
+    
+    os.chdir(dir)
+
     for file in glob.glob("*.png"):
         filePath = dir + '/' + file
+        print(filePath)
+
         images[file] = imread(filePath, as_gray=True)
-        if(trainDir):
+        evalData['classes'] = None
 
-            trainData['targets'].append(file[:4:])
-
-            fd, img = hog(images[file],
-                   orientations=8,
-                   pixels_per_cell=(8, 8),
-                   cells_per_block=(1, 1),
-                   block_norm='L1',
-                   visualize=True)
-            trainData['img_hog'].append(img)
-            trainData['img_hog_d'].append(fd)
-        else:
-            testData['targets'].append(file[:4:])
-
-            fd, img = hog(images[file],
-                   orientations=8,
-                   pixels_per_cell=(8, 8),
-                   cells_per_block=(1, 1),
-                   block_norm='L1',
-                   visualize=True)
-            testData['img_hog'].append(img)
-            testData['img_hog_d'].append(fd)
-
-if __name__ == "__main__":
-
-    currDirectory = os.getcwd()
-    trainDirectory = currDirectory + '/trainImages'
-    devDirectory = currDirectory + '/devImages'
-
-    loadTrainDirData(trainDirectory, True)
-    loadTrainDirData(devDirectory, False)
-
-    X_trainArr = trainData['img_hog_d']
-    y_trainArr = trainData['targets']
+        fd, img = hog(images[file],
+            orientations=8,
+            pixels_per_cell=(8, 8),
+            cells_per_block=(1, 1),
+            block_norm='L1',
+            visualize=True)
+        evalData['img_hog'].append(img)
+        evalData['img_hog_d'].append(fd)
     
-    X_testData = testData['img_hog_d']
-    y_testData = testData['targets']
+
+def trainModel():
+    X_trainArr = trainData['img_hog_d']
+    y_trainArr = trainData['classes']
+
+    X_testDataArr = testData['img_hog_d']
+    y_testDataArr = testData['classes']
 
     start_time = time()
     classifier.fit(X_trainArr, y_trainArr)
     print('\n\tTraining time:', time() - start_time)
 
-    print('\tScore:', classifier.score(X_test, y_test))
+    print('\tScore:', classifier.score(X_testDataArr, y_testDataArr))
+
+
+if __name__ == "__main__":
+
+    print(sys.argv)
+
+    training = False
+
+    if(len(sys.argv) == 2):
+        if(sys.argv[1] == '--train'):
+            training = True
+
+    currDirectory = os.getcwd()
+
+    trainDirectory = currDirectory + '/trainImages'
+    devDirectory = currDirectory + '/devImages'
+    evalDirectory = currDirectory + '/evalImages'
+
+    if(training):
+        loadTrainDirImages(trainDirectory, True)
+        loadTrainDirImages(devDirectory, False)
+        trainModel()
+    else:
+        loadEvalDirImages(evalDirectory)
+
+    X_evalDataArr = evalData['img_hog_d']
+    Y_evalDataArr = evalData['classes'] 
+
+    
